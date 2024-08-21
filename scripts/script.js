a7416bf4682c27244f71cf39642fe2d8a333d827
timdvd <76249593+timdvd@users.noreply.github.com>
2024-08-21 07:19:24 +0300

    commit

---
+++ scripts/script.js
@@ -0,0 +1,89 @@
+function copyMenu(){
+  let dptCategory = document.querySelector('.dpt-cat');
+  let dptPlace = document.querySelector('.departments');
+  dptPlace.innerHTML = dptCategory.innerHTML;
+
+  let mainNav = document.querySelector('.header-nav nav');
+  let navPlace = document.querySelector('.off-canvas nav');
+  navPlace.innerHTML = mainNav.innerHTML;
+
+  let topNav = document.querySelector('.header-top .wrapper');
+  var topPlace = document.querySelector('.off-canvas .thetop-nav');
+  topPlace.innerHTML = topNav.innerHTML;
+
+}
+copyMenu();
+
+const menuButton = document.querySelector('.trigger'),
+      closeButton = document.querySelector('.t-close'),
+      addclass = document.querySelector('.site');
+menuButton.addEventListener('click', function(){
+  addclass.classList.toggle('showmenu');
+});
+closeButton.addEventListener('click', function(){
+  addclass.classList.remove('showmenu')
+});
+
+
+const submenu = document.querySelectorAll('.has-child .icon-small');
+submenu.forEach((menu) => menu.addEventListener('click', toggle));
+function toggle(e){
+  e.preventDefault();
+  submenu.forEach((item) => item != this ? item.closest('.has-child').classList.remove('expand') : null);
+  if (this.closest('.has-child').classList != 'expand');
+  this.closest('.has-child').classList.toggle('expand')
+}
+
+const swiper = new Swiper('.swiper', {
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+});
+
+// show menu
+
+const dptButton = document.querySelector('.dpt-cat .dpt-trigger'),
+      dptClass = document.querySelector('.site');
+dptButton.addEventListener('click', function(){
+  dptClass.classList.toggle('showdpt');
+});
+
+// products image slider
+let productThumb = new Swiper('.small-image', {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 3,
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    480: {
+      spaceBetween: 32,
+
+    }
+  }
+});
+let productBig = new Swiper('.big-image', {
+  loop: true,
+  autoHeight: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: productThumb,
+  }
+})
+
+// show search
+const searchButton = document.querySelector('.t-search'),
+      tClose = document.querySelector('.search-close'),
+      showClass = document.querySelector('.site');
+searchButton.addEventListener('click', function(){
+  showClass.classList.toggle('showsearch');
+})
+tClose.addEventListener('click', function(){
+  showClass.classList.remove('showsearch');
+})
